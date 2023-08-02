85e4efc0ec77ef0e2545a31bc151731b688b4ac3
Al Tworowsky <al@joinquantum.com.au>
2023-08-02 19:32:57 +1000

    Added custom font

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
 
+const CLERK_PUBLISHABLE_KEY = "pk_test_Z2xvd2luZy13b2xmLTI3LmNsZXJrLmFjY291bnRzLmRldiQ"
 export default function App() {
+  const [loaded] = useFonts({
+    'Inter-Medium': require('./assets/Inter-Medium.otf')
+  })
+
+  if (!loaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <SafeAreaView style={styles.container}>
+        <Text style={{ fontFamily: 'Inter-Medium', fontSize: 30 }}>Hello world!</Text>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
